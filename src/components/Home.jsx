9125908c5f7ab92c9770fec7fd3a4b343cee1fc8
Anthony Saliba <anthonysaliba93@gmail.com>
2024-05-11 22:10:58 +0300

    Finalize the homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
 import { PiIdentificationBadgeFill } from "react-icons/pi";
+import { useRef } from "react";
 import Background from "../assets/bg.png";
 import AstraProperties from "../assets/astrap.png";
 import LunaCloud from "../assets/lunac.png";
 import QuickNews from "../assets/quickn.png";
-import { useRef } from "react";
+import { RiReactjsFill, RiTailwindCssFill } from "react-icons/ri";
+import { MdEmail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
 
 function Hero() {
   const targetRef = useRef(null);
@@ -20,6 +23,10 @@ function Hero() {
 
   function openLink(url) {
     window.open(url, "_blank");
+  }
+
+  function mailToEmail(email) {
+    window.location.href = `mailto:${email}`;
   }
 
   return (
@@ -102,9 +109,8 @@ function Hero() {
                   Luna Cloud
                 </h1>
                 <p className="text-base text-black font-normal drop-shadow-md px-4">
-                  Modern real estate website built with React & CSS, with a
-                  primary focus on good responsive practices while using no
-                  media queries.
+                  Minimalistic cloud storage website built with React & Tailwind
+                  CSS, with a heavy focus on ease-of-use and beautiful design.
                 </p>
               </div>
               <div className="w-full flex flex-row justify-center items-center px-12 my-4 gap-4">
@@ -130,9 +136,9 @@ function Hero() {
                   Quick News
                 </h1>
                 <p className="text-base text-black font-normal drop-shadow-md px-4">
-                  Modern real estate website built with React & CSS, with a
-                  primary focus on good responsive practices while using no
-                  media queries.
+                  Modern news website built with React & Tailwind that leverages
+                  NewsAPI for providing users with a quick and easy way to get
+                  updated news.
                 </p>
               </div>
               <div className="w-full flex flex-row justify-center items-center px-12 my-4 gap-4">
@@ -147,6 +153,49 @@ function Hero() {
               </div>
             </div>
           </div>
+        </div>
+        {/* About Section */}
+        <div
+          className={`w-full min-h-[14rem] flex flex-col justify-center items-center text-center px-4 sm:px-20 py-8 gap-2`}
+        >
+          <div className="max-w-[900px] flex flex-col justify-center items-center gap-2 shadow-xl place-items-center rounded-md hover:scale-[1.02] duration-300">
+            <div className="w-full flex flex-col justify-center items-center gap-2 my-12">
+              <h1 className="text-2xl text-black font-bold uppercase drop-shadow-md">
+                About
+              </h1>
+              <p className="text-lg md:text-xl text-black font-normal drop-shadow-md px-4">
+                I am a fresh graduate front end web developer that loves making
+                beautiful and modern websites, while always aspiring to learn
+                and advance my knowledge whenever possible.
+              </p>
+              <div className="w-full flex flex-row justify-center items-center text-center gap-6 py-4">
+                <MdEmail
+                  onClick={() => mailToEmail("anthonysaliba93@gmail.com")}
+                  size={35}
+                  className="cursor-pointer hover:scale-110 duration-300"
+                />
+                <FaGithub
+                  onClick={() =>
+                    openLink("https://github.com/AnthonySaliba787")
+                  }
+                  size={35}
+                  className="cursor-pointer hover:scale-110 duration-300"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Fact Section */}
+        <div
+          className={`w-full min-h-[7rem] bg-black flex flex-col justify-center items-center text-center px-4 sm:px-20 py-4 gap-2`}
+        >
+          <div className="w-full text-white flex flex-row justify-center items-center text-center gap-4">
+            <RiReactjsFill size={50} />
+            <RiTailwindCssFill size={50} />
+          </div>
+          <h1 className="text-lg text-white md:text-xl lg:text-2xl font-bold uppercase drop-shadow-md">
+            This website was made from scratch with react & tailwind CSS
+          </h1>
         </div>
       </div>
     </>
