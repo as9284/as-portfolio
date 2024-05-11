00f74f1f5d7dd8ddf1f3953d5852eca54e9af07f
Anthony Saliba <anthonysaliba93@gmail.com>
2024-05-11 15:27:35 +0300

    Minor design improvements for hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ function Hero() {
         <div
           className={`w-full min-h-screen bg-neutral-700 flex flex-col justify-center items-center text-center py-4 gap-1 md:gap-4 lg:gap-6`}
         >
-          <div className="max-w-[1240px] h-96 flex flex-col justify-center items-center px-12 mx-4 bg-blue-400 rounded-md border-2 border-blue-300 shadow-xl hover:scale-[1.02] hover:shadow-2xl duration-300">
+          <div className="max-w-[1240px] h-[30rem] flex flex-col justify-center items-center px-12 mx-4 bg-blue-400 rounded-md border-2 border-blue-300 shadow-xl hover:scale-[1.02] hover:shadow-2xl duration-300">
             <div className="w-full flex flex-col justify-center items-center gap-2 my-12">
               <PiIdentificationBadgeFill
                 size={75}
@@ -26,7 +26,7 @@ function Hero() {
               </p>
             </div>
 
-            <button className="text-sm md:text-lg px-12 py-4 my-2 bg-neutral-900 rounded-md font-medium text-white shadow-sm hover:bg-neutral-700 hover:shadow-lg active:scale-95 duration-300 uppercase">
+            <button className="text-base md:text-lg lg:text-xl px-12 py-4 my-2 bg-neutral-900 rounded-md font-medium text-white shadow-sm hover:bg-neutral-700 hover:shadow-lg focus:bg-neutral-700 focus:shadow-lg active:scale-95 duration-300 uppercase">
               Discover
             </button>
           </div>
