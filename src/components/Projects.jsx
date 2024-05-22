e7c7d81f17f57b9335187d8aaa50a76bf8669ec6
Anthony Saliba <anthonysaliba93@gmail.com>
2024-05-22 14:27:09 +0300

    Implement functionality for switching between active websites

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,6 +9,7 @@ import QuickPass from "../assets/quickpass.webp";
 import QuickShorten from "../assets/quickshorten.webp";
 import AstraFeedback from "../assets/astrafeedback.webp";
 import AstraWeather from "../assets/astraweather.webp";
+import { useEffect, useState } from "react";
 
 const images = {
   "astrap.webp": AstraProperties,
@@ -32,6 +33,22 @@ const importAll = (imagePaths) => {
 const importedImages = importAll(images);
 
 function Projects({ projectsRef }) {
+  const [activeType, setActiveType] = useState("static");
+
+  if (!localStorage.getItem("activeType")) {
+    localStorage.setItem("activeType", "static");
+  }
+
+  const changeActiveType = (type) => {
+    setActiveType(type);
+    localStorage.setItem("activeType", type);
+  };
+
+  useEffect(() => {
+    const savedType = localStorage.getItem("activeType");
+    setActiveType(savedType);
+  }, []);
+
   return (
     <>
       <div
@@ -42,24 +59,42 @@ function Projects({ projectsRef }) {
           Projects
         </h1>
         <div className="w-full flex flex-row justify-center items-center gap-4 text-center">
-          <button className="w-1/2 px-4 py-4 bg-neutral-800 rounded-md shadow-md text-neutral-200 font-medium uppercase hover:bg-neutral-700 hover:shadow-xl focus:bg-neutral-700 focus:shadow-lg active:scale-95 duration-300">
+          <button
+            onClick={() => changeActiveType("static")}
+            className={
+              activeType === "static"
+                ? "w-1/2 px-4 py-4 bg-neutral-600 rounded-md shadow-md text-neutral-200 font-medium uppercase hover:bg-neutral-700 hover:shadow-xl focus:bg-neutral-600 focus:shadow-lg active:scale-95 duration-300"
+                : "w-1/2 px-4 py-4 bg-neutral-800 rounded-md shadow-md text-neutral-200 font-medium uppercase hover:bg-neutral-700 hover:shadow-xl focus:bg-neutral-600 focus:shadow-lg active:scale-95 duration-300"
+            }
+          >
             Static Websites
           </button>
-          <button className="w-1/2 px-4 py-4 bg-neutral-800 rounded-md shadow-md text-neutral-200 font-medium uppercase hover:bg-neutral-700 hover:shadow-xl focus:bg-neutral-700 focus:shadow-lg active:scale-95 duration-300">
+          <button
+            onClick={() => changeActiveType("dynamic")}
+            className={
+              activeType === "dynamic"
+                ? "w-1/2 px-4 py-4 bg-neutral-600 rounded-md shadow-md text-neutral-200 font-medium uppercase hover:bg-neutral-700 hover:shadow-xl focus:bg-neutral-600 focus:shadow-lg active:scale-95 duration-300"
+                : "w-1/2 px-4 py-4 bg-neutral-800 rounded-md shadow-md text-neutral-200 font-medium uppercase hover:bg-neutral-700 hover:shadow-xl focus:bg-neutral-600 focus:shadow-lg active:scale-95 duration-300"
+            }
+          >
             Dynamic Websites
           </button>
         </div>
         <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {CardData.map((item) => (
-            <Card
-              key={item.id}
-              image={importedImages[item.src]}
-              title={item.title}
-              description={item.description}
-              link={item.link}
-              tags={item.tags}
-            />
-          ))}
+          {CardData.map((item) => {
+            if (item.type === activeType) {
+              return (
+                <Card
+                  key={item.id}
+                  image={importedImages[item.src]}
+                  title={item.title}
+                  description={item.description}
+                  link={item.link}
+                  tags={item.tags}
+                />
+              );
+            }
+          })}
         </div>
       </div>
     </>
